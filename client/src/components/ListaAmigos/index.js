3ae48f129ff0b3ede864600d7bada0fca8f5b7d7
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-11-30 11:37:08 -0300

    Corrige bug no overflow da lista de amigos

--- client/src/components/ListaAmigos/index.js
+++ client/src/components/ListaAmigos/index.js
@@ -33,66 +33,64 @@ const ListaAmigos = ({
   }, [friendList, filterQuery]);
 
   return (
-    <div className=" col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-sm-12 my-profile-page-content-container-sizing">
-      <div className="people-nearby">
-        <h3 className="mb-3 text-center h4 my-blue-1 my-bolder">Amigos</h3>
+    <div className="col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-sm-12 people-nearby my-profile-page-content-container-sizing">
+      <h3 className="mb-3 text-center h4 my-blue-1 my-bolder">Amigos</h3>
 
-        {friendList.length > 0 && (
-          <input
-            placeholder="Filtrar por nome..."
-            value={filterQuery}
-            onChange={(e) => setFilterQuery(e.target.value)}
-            className="form-control my-1 p-2"
-            type="text"
-            name="filter-friends"
-          />
-        )}
+      {friendList.length > 0 && (
+        <input
+          placeholder="Filtrar por nome..."
+          value={filterQuery}
+          onChange={(e) => setFilterQuery(e.target.value)}
+          className="form-control my-1 p-2"
+          type="text"
+          name="filter-friends"
+        />
+      )}
 
-        <div className="people-nearby pt-0 overflow-auto h-75">
-          {(loadingAuthUserFriendList && <i className="fas fa-spinner" />) || (
-            <>
-              {(filteredFriendList.length > 0 &&
-                filteredFriendList
-                  .sort((a, b) =>
-                    `${a.nome} ${a.sobrenome}`.localeCompare(
-                      `${b.nome} ${b.sobrenome}`
-                    )
+      <ul className="people-nearby pt-0 overflow-auto h-75">
+        {(loadingAuthUserFriendList && <i className="fas fa-spinner" />) || (
+          <>
+            {(filteredFriendList.length > 0 &&
+              filteredFriendList
+                .sort((a, b) =>
+                  `${a.nome} ${a.sobrenome}`.localeCompare(
+                    `${b.nome} ${b.sobrenome}`
                   )
-                  .map((friend) => (
-                    <CardAmigo
-                      key={friend.idUsuario}
-                      userInfo={friend}
-                      mode="FRIENDLIST"
-                      authUserFriendlistIds={authUserFriendlistIds}
-                      loadingAuthUserFriendList={loadingAuthUserFriendList}
-                    />
-                  ))) ||
-                (friendList.length > 0 && (
-                  <p className="text-center">Nenhum amigo encontrado</p>
-                ))}
-            </>
-          )}
-          {!loadingAuthUserFriendList && friendList.length < 1 && (
-            <>
-              {(id && (
-                <p className="text-center">
-                  Ainda não tem nenhum amigo{" "}
-                  <span role="img" aria-label="Emoji Triste">
-                    😔
-                  </span>
-                </p>
-              )) || (
-                <p className="text-center">
-                  Você ainda não tem nenhum amigo{" "}
-                  <span role="img" aria-label="Emoji Triste">
-                    😔
-                  </span>
-                </p>
-              )}
-            </>
-          )}
-        </div>
-      </div>
+                )
+                .map((friend) => (
+                  <CardAmigo
+                    key={friend.idUsuario}
+                    userInfo={friend}
+                    mode="FRIENDLIST"
+                    authUserFriendlistIds={authUserFriendlistIds}
+                    loadingAuthUserFriendList={loadingAuthUserFriendList}
+                  />
+                ))) ||
+              (friendList.length > 0 && (
+                <p className="text-center">Nenhum amigo encontrado</p>
+              ))}
+          </>
+        )}
+        {!loadingAuthUserFriendList && friendList.length < 1 && (
+          <>
+            {(id && (
+              <p className="text-center">
+                Ainda não tem nenhum amigo{" "}
+                <span role="img" aria-label="Emoji Triste">
+                  😔
+                </span>
+              </p>
+            )) || (
+              <p className="text-center">
+                Você ainda não tem nenhum amigo{" "}
+                <span role="img" aria-label="Emoji Triste">
+                  😔
+                </span>
+              </p>
+            )}
+          </>
+        )}
+      </ul>
     </div>
   );
 };
